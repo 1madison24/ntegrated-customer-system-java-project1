862d4ed6e5f066abf2152b038ae5e1c0d672ae8a
Madison Sargeant <madison.sargeant@uconn.edu>
2021-10-13 14:30:09 -0400

    Created CustomerProf class

--- src/CustomerProf.java
+++ src/CustomerProf.java
@@ -1,3 +1,86 @@
+//import vehicleInfo;
+public class CustomerProf {
+    private String adminID;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phone;
+    private float income;
+    private String status;
+    private String use;
+    //private class vehicleInfo;
+
+    public CustomerProf() {
+        super();
+    }
+    public CustomerProf(String adminID, String firstName, String lastName, String address, String phone,
+                        float income, String status, String use) {
+        super();
+        this.adminID = adminID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+        this.income = income;
+        this.status = status;
+        this.use = use;
+        //this.vehicleInfo = vehicleInfo;
+
+    }
+    public String getadminID() {
+        return adminID;
+    }
+    public String getfirstName() {
+        return firstName;
+    }
+    public void UpdatefirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getlastName() {
+        return lastName;
+    }
+    public void UpdatelastName(String lastName){
+        this.lastName = lastName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void UpdateAddress(String address){
+        this.address = address;
+    }
+    public String getphone() {
+        return phone;
+    }
+    public void Updatephone(String phone){
+        this.phone = phone;
+    }
+    public float getincome() {
+        return income;
+    }
+    public void Updateincome(float income){
+        this.income = income;
+    }
+    public String getstatus() {
+        return status;
+    }
+    public void Updatestatus(String status){
+        this.status = status;
+    }
+    public String getuse() {
+        return use;
+    }
+    //public String getvehicleInfo() {
+    //    return vehicleInfo;
+    //}
+    //public void UpdatevehicleInfo(class vehicleInfo){
+    // this.vehicleInfo = vehicleInfo;
+    //}
+    public String toString() {
+        return "CustomerProf [adminID =" + adminID + ", firstName =" + firstName + ", lastName =" + lastName +
+                ", address =" + address + ", phone =" + phone + ", income =" + income +
+                ", status =" + status + ", use =" + use + "]";
+    }
+}
 
 
 
