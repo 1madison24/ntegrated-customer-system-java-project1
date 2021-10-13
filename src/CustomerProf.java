7ac3ff1cbbb434aed0e406245ba96071f16be1be
Ashely Tang <ashely.tang@uconn.edu>
2021-10-13 00:17:46 -0400

    finished creating CustomerProf class

--- src/CustomerProf.java
+++ src/CustomerProf.java
@@ -1,2 +1,97 @@
+// keeps track of different characteristics of a customer's profile, i.e., name, address, phone number, number of years
+// since the individuals is a customer, customer status (active or inactive), whether the vehicle is for personal use or both
 public class CustomerProf {
+    private String adminID;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phone;
+    private float income;
+    private String status;
+    private String use;
+    private VehicleInfo vehicleInfo; //??? is this right?
+
+    public CustomerProf(String custAdminID, String custFirstName, String custLastName, String custAddress, String custPhone,
+                        float custIncome, String custStatus, String custUse) {
+        adminID = custAdminID;
+        firstName = custFirstName;
+        lastName = custLastName;
+        address = custAddress;
+        phone = custPhone;
+        income = custIncome;
+        status = custStatus;
+        use = custUse;
+    }
+
+    public String getadminID() {
+        return adminID;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public float getIncome() {
+        return income;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public String getUse() {
+        return use;
+    }
+    public VehicleInfo getVehicleInfo() {
+        return vehicleInfo;
+    }
+    public void updateFirstName(String newFirstName) {
+        firstName = newFirstName;
+    }
+    public void updateLastName(String newLastName) {
+        lastName = newLastName;
+    }
+    public void updateAddress(String newAddress) {
+        address = newAddress;
+    }
+    public void updatePhone(String newPhone) {
+        phone = newPhone;
+    }
+    public void updateIncome (float newIncome) {
+        income = newIncome;
+    }
+    public void updateStatus(String newStatus) {
+        status = newStatus;
+    }
+    public void updateVehicleInfo(VehicleInfo newVehicleInfo) {
+        vehicleInfo = newVehicleInfo;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
