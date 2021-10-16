9d032b787fc2bfaf6712ac2a3bf2d0c492ad7834
Ashely Tang <ashely.tang@uconn.edu>
2021-10-15 23:34:27 -0400

    creating the deleteProfile class

--- src/CustomerProfDB.java
+++ src/CustomerProfDB.java
@@ -17,6 +17,23 @@ public class CustomerProfDB {
         numCustomer++;                //increment the number of customers by 1
     }
     public boolean deletePofile(String adminID, String lastName) {
+        boolean success = false;      //set success to false until the profile is deleted. Then set success to true
+        for (int i = 0; i <customerList.size(); i++) {
+            CustomerProf customer = customerList.get(i);    //iterate through each person in the customer profile list
+
+            //now if the adminID == last name, the matching customerprofile has been found
+            if((customer.getadminID().equals(adminID)) && (customer.getlastName().equals(lastName))) {
+                currentCustomerIndex = i;
+                break;
+            }
+        }
+        if(currentCustomerIndex == -1) {
+            System.out.println("No such customer in the database.");
+            return null;
+        }
+        else{ //return that matching customer profile
+            return customerList.get(currentCustomerIndex);
+        }
 
     }
     public CustomerProf findProfile(String adminID, String lastName) {
