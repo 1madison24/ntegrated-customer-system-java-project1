02244db556b50322c687e1d00a929fa75fbca521
Madison Sargeant <madison.sargeant@uconn.edu>
2021-10-20 13:54:48 -0400

    Changes made to the CustomerDB

--- src/CustomerProfDB.java
+++ src/CustomerProfDB.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.io.Serializable;
+import CustomerProf.CustomerProf;
 /* Object serialization = object can be represented as a sequence of bytes that includes
     the object's data, info about the object's type, and the types of data stores in the object
    After serialized object is written into a file, it can be read from that file and
