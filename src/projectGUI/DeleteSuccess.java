1a9aa2f9a761ece8aa88a4da29a4c96263f74fc0
Madison Sargeant <madison.sargeant@uconn.edu>
2021-11-16 16:06:26 -0500

    Created the DeleteSuccess GUI

--- src/projectGUI/DeleteSuccess.java
+++ src/projectGUI/DeleteSuccess.java
@@ -1,4 +1,25 @@
 package projectGUI;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class DeleteSuccess {
+    private JPanel delSuccess;
+    private JButton OKButton;
+
+    public DeleteSuccess(){
+        OKButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+    public void openGUI(){
+        JFrame f = new JFrame("Integrated Customer System");
+        f.setContentPane(new DeleteSuccess().delSuccess);
+        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        f.pack();
+        f.setVisible(true);
+    }
 }
