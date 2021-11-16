da1e9eb926a3e5d28651af1239d34cc3a0615dce
Madison Sargeant <madison.sargeant@uconn.edu>
2021-11-16 15:45:03 -0500

    Created the DeleteFail GUI

--- src/projectGUI/DeleteFail.java
+++ src/projectGUI/DeleteFail.java
@@ -1,4 +1,26 @@
 package projectGUI;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class DeleteFail {
+    private JPanel deleteFail;
+    private JButton OKButton;
+
+    public DeleteFail(){
+        OKButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                System.exit(0);
+            }
+        });
+    }
+    public void openGUI(){
+        JFrame f = new JFrame("Integrated Patient System");
+        f.setContentPane(new DeleteFail().deleteFail);
+        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        f.pack();
+        f.setVisible(true);
+    }
 }
