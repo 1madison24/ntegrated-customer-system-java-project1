eac0475d203f63a763f3293323fa434427f3181f
Ashely Tang <65383136+ashelytang@users.noreply.github.com>
2021-12-08 17:01:48 -0500

    add error handling

--- src/VehicleInfo/VehicleInfo.java
+++ src/VehicleInfo/VehicleInfo.java
@@ -1,10 +1,17 @@
 package VehicleInfo;
+import java.util.*;
 //keeps track of the details of the vehicle owned by the customer
 public class VehicleInfo {
     private String model;   //name of the vehicle model
     private String year;    //year in which the vehicle was manufactured
     private String type;    //type of vehicle, must select from: sedan, hatchback, luxury, sport, other
     private String method;  //how the vehicle was acquired -- must select from: new certified pre-owned, used, other
+
+
+    //will utilize these HashSets for boolean values during conditional statements
+    private HashSet<String> validType = new HashSet<>(Arrays.asList("sedan", "hatchback", "luxury", "sport", "other"));
+    private HashSet<String> validMethod = new HashSet<>(Arrays.asList("new", "certified pre-owned", "used", "other"));
+
 
     public VehicleInfo(String vehicle_model, String vehicle_year, String vehicle_type, String vehicle_method) {
         model = vehicle_model;
@@ -34,15 +41,37 @@ public class VehicleInfo {
     }
 
     public void updateYear(String NewYear) { //reassigns the year variable to the new one
-        year = NewYear;
+        try{
+            if((Integer.parseInt(NewYear) > 999) && (Integer.parseInt(NewYear) < 10000)) {
+                year = NewYear;
+            }
+            else {
+                throw new IllegalArgumentException(("Year is invalid: Correct format should have 4 digits: e.x 2022"));
+            }
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException(("Year is invalid: Correct format should have 4 digits: e.x 2022"));
+        }
     }
 
     public void updateType(String NewType) { //reassigns the type variable to the new one
-        type = NewType;
+        if (validType.contains(NewType.toLowerCase(Locale.ROOT))) {
+            type = NewType.toLowerCase(Locale.ROOT);
+        }
+        else {
+            throw new IllegalArgumentException("Type needs to be one of the following: sedan, hatchback, luxury, sport, other");
+        }
     }
 
     public void updateMethod(String NewMethod) { //reassigns the method variable to the new one
-        method = NewMethod;
+        if(validMethod.contains(NewMethod.toLowerCase(Locale.ROOT))) {
+            method = NewMethod;
+        }
+        else {
+            throw new IllegalArgumentException("Model needs to be one of these: new, certified, pre-owned, used, other");
+        }
+
+
     }
 }
 //does not return anything, but updates the vehicle information
