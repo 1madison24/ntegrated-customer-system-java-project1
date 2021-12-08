0ab03a3f0e97ce8b070bbdb4c814ceec9b57c165
1madison24 <92468456+1madison24@users.noreply.github.com>
2021-12-08 17:01:10 -0500

    Many changes made, profiles appear

--- src/projectGUI/DispProfile1.java
+++ src/projectGUI/DispProfile1.java
@@ -1,16 +1,17 @@
 package projectGUI;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.nio.file.Path;
 import java.util.List;
 
 public class DispProfile1 {
-    private JPanel dispProf1 = new JPanel();
-    private JTextField lastName = new JTextField(10);
-    private JTextField adminID = new JTextField(10);
-    private JButton selectButton = new JButton("Select");
+    private final JPanel dispProf1 = new JPanel();
+    private final JTextField lastName = new JTextField(10);
+    private final JTextField adminID = new JTextField(10);
+    private final JButton selectButton = new JButton("Select");
 
     public DispProfile1(){
         selectButton.addActionListener(new ActionListener() {
@@ -21,12 +22,22 @@ public class DispProfile1 {
                 DBController funcCall = new DBController();
                 getPath pathname = new getPath();
                 Path path = pathname.getIt();
-                boolean status = funcCall.searchProfile(last, id, path);
-
+                boolean status = DBController.searchProfile(last, id, path);
                 if(status) {
-                    List<String> theProf = funcCall.getProfileByLastName(last, id, pathname.getIt());
-                    DispForm2 prof = new DispForm2(theProf);
-                    prof.openGUI(theProf);
+                    funcCall.getProfileByLastName(last,id,path);
+
+                    List<String> theProf = funcCall.getProfileByLastName(last, id, path);
+                    JFrame DispForm = new JFrame();
+                    DispForm.setLayout(new GridLayout());
+                    JLabel a = new JLabel("<html> Admin ID: " + theProf.get(0) + "<br/>First Name: " + theProf.get(1)
+                            + "<br/>Last Name: " + theProf.get(2) + "<br/>Address: " + theProf.get(3) + "<br/>Phone: " + theProf.get(4) +
+                            "<br/>Income: " + theProf.get(5) + "<br/>Use: " + theProf.get(6) + "<br/>Status: " + theProf.get(7) +
+                            "<br/>Model: " + theProf.get(8) + "<br/>Year: " + theProf.get(9) + "<br/>Type: " + theProf.get(10) +
+                            "<br/>Method: " + theProf.get(11) + "</html>");
+                    DispForm.add(a);
+                    DispForm.pack();
+                    DispForm.setVisible(true);
+
                 }
                 else {
                     JOptionPane.showMessageDialog(null, "There is no such Profile");
