2a660b76ad6998bd6ffca2ed96fe168af147730f
1madison24 <92468456+1madison24@users.noreply.github.com>
2021-12-08 20:51:28 -0500

    Changed 10 warnings to 4

--- src/projectGUI/DeleteProfile.java
+++ src/projectGUI/DeleteProfile.java
@@ -1,34 +1,31 @@
 package projectGUI;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
 import java.nio.file.Path;
 
 public class DeleteProfile {
-    private JPanel deletePanel = new JPanel();
-    private JTextField adminID = new JTextField(10);
-    private JTextField lastName = new JTextField(10);
-    private JButton submit = new JButton("Submit");
+    private final JPanel deletePanel = new JPanel();
+    private final JTextField adminID = new JTextField(10);
+    private final JTextField lastName = new JTextField(10);
+    private final JButton submit = new JButton("Submit");
 
     public DeleteProfile(){
-        submit.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e){
-                String last = lastName.getText();
-                String id = adminID.getText();
+        submit.addActionListener(e -> {
+            String last = lastName.getText();
+            String id = adminID.getText();
 
-                DBController funcCall = new DBController();
-                getPath pathname = new getPath();
-                Path path = pathname.getIt();
-                boolean status = funcCall.searchProfile(last, id, path);
+            DBController funcCall = new DBController();
+            getPath pathname = new getPath();
+            Path path = getPath.getIt();
+            boolean status = DBController.searchProfile(last, id, path);
 
-                if (status){
-                    funcCall.deleteProfileByLastname(last, id, path);
-                    JOptionPane.showMessageDialog(null, "Success!");
-                }
-                else{
-                    JOptionPane.showMessageDialog(null, "The Profile does not exist");
-                }
+            if (status){
+                DBController.deleteProfileByLastname(last, id, path);
+                JOptionPane.showMessageDialog(null, "Success!");
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "The Profile does not exist");
             }
         });
     }
