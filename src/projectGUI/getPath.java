5ed5d986c1188d3e533dd1f910427f6b295247a7
1madison24 <92468456+1madison24@users.noreply.github.com>
2021-12-09 20:00:20 -0500

    Commenting code

--- src/projectGUI/getPath.java
+++ src/projectGUI/getPath.java
@@ -4,6 +4,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class getPath {
+    //Creates a path to the Database.txt file for the GUIs to write to 
     public static Path thePath = Paths.get("src/dbTestingText.txt");
     public static Path getIt(){
         return thePath;
